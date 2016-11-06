66669eec071c57d3d4d30a4b82d2ba552f8a81be
Gabor Szarnyas <szarnyasg@gmail.com>
2016-11-05 23:55:21 -0400

    Enhanced benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NEO4J_SHELL="$NEO4J_HOME/bin/neo4j-shell -file"
-METRICS=(c dc1 dimensional-degree dimensional-metrics mpc na)
 
-for metric in ${METRICS[@]}; do
+for metric in *.cyp; do
+  metric="${metric%.*}"
   echo $metric
-  time $NEO4J_SHELL $metric.cyp 2>&1 > /dev/null| grep real
+  /usr/bin/time -f "%e" $NEO4J_SHELL $metric.cyp 2>&1 > /dev/null
 done
